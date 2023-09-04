f91749bcfe57b8131ec9df72527707dc35ab7f6c
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2023-09-04 12:42:15 +0100

    Implemented Pun, Needed to player sync

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+public class Constants
+{
+    public readonly static string loadingScene="LoadingScene";
+    public readonly static string lobbyScene = "LobbyScene";
+    public readonly static string gameScene = "GameScene";
+
+}
+   
+
---
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+
+    public SettingsData createSettings;
+    public SettingsData joinSettings;
+
+    private void Start()
+    {
+        createSettings.button.onClick.AddListener(() => CreateRoom());
+        joinSettings.button.onClick.AddListener(() => JoinRoom());
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Room Created");
+        PhotonNetwork.CreateRoom(createSettings.inputField.text);
+    }
+
+    public void JoinRoom()
+    {
+        Debug.Log("Joined Room");
+        PhotonNetwork.JoinRoom(joinSettings.inputField.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(Constants.gameScene);
+    }
+
+}
+
+[System.Serializable]
+public class SettingsData
+{
+    public Button button;
+    public InputField inputField;
+}
+
